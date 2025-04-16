ee28cc4521d84ad3e31a1219a33d2c57cfa88fee
Alex Baranowski <Alex@euro-linux.com>
2025-04-16 14:44:51 +0200

    Debug for rabbitmq-4-management build

--- containers/images/rabbitmq-4-management/init.sh
+++ containers/images/rabbitmq-4-management/init.sh
@@ -7,7 +7,13 @@
 
 set -eu
 
-RABBITMQ_VERSION=$(docker run --rm -it sourcemation/rabbitmq-4:latest env |grep APP_VERSION |awk -F'=' '{print $2}' | tr -d '\r')
+echo "Pulling RabbitMQ 4 image..."
+docker pull sourcemation/rabbitmq-4:latest
+echo "RabbitMQ 4 image pulled successfully. Checking version from the image..."
+
+RABBITMQ_VERSION=$(docker run --rm -it sourcemation/rabbitmq-4:latest env | grep APP_VERSION |awk -F'=' '{print $2}' | tr -d '\r')
+
+echo "Version is '$RABBITMQ_VERSION'"
 
 if [[ ! "$RABBITMQ_VERSION" =~ ^4\. ]]; then
     echo "Version 4 is no more! Check the EOL status at https://endoflife.date/rabbitmq"
