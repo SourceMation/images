26c4bb2487f1bef5c537795a435cb703aef5dab4
Radosław Kolba <radoslaw.kolba@linuxpolska.com>
2025-04-29 15:45:11 +0200

    fixes on init.sh

--- containers/images/redis-operator/init.sh
+++ containers/images/redis-operator/init.sh
@@ -11,7 +11,6 @@ set -euo pipefail
 REDIS_OPERATOR_GIT="https://github.com/OT-CONTAINER-KIT/redis-operator.git"
 VERSION=$(git ls-remote --tags $REDIS_OPERATOR_GIT | grep -o 'v.*' | sort --version-sort --reverse | head -1)
 
-sed -i "s/REDIS_OPERATOR_VERSION=\"[^\"]*\"/REDIS_OPERATOR_VERSION=\"$VERSION\"/" Dockerfile || exit 1
 sed -i "s/version=\"[^\"]*\"/version=\"${VERSION:1}\"/" Dockerfile || exit 1
 
-echo "Finished setting up the Redis Exporter ${VERSION:1} image"
+echo "Finished setting up the Redis Operator ${VERSION:1} image"
