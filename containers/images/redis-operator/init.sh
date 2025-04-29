7cf2f96b117b3a6c25582dd0dcf883d1f116f6c2
Radosław Kolba <radoslaw.kolba@linuxpolska.com>
2025-04-29 11:58:37 +0200

    redis-operator init

---
+++ containers/images/redis-operator/init.sh
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env bash
+# ---------------------------------------------------
+# Automated build process for the redis image
+# Author: Radosław Kolba
+# e-mail: radoslaw.kolba@linuxpolska.pl
+# ---------------------------------------------------
+
+set -euo pipefail
+
+REDIS_OPERATOR_GIT="https://github.com/OT-CONTAINER-KIT/redis-operator.git"
+VERSION=$(git ls-remote --tags $REDIS_OPERATOR_GIT | grep -o 'v.*' | sort --version-sort --reverse | head -1)
+
+sed -i "s/REDIS_OPERATOR_VERSION=\"[^\"]*\"/REDIS_OPERATOR_VERSION=\"$VERSION\"/" Dockerfile || exit 1
+sed -i "s/version=\"[^\"]*\"/version=\"${VERSION:1}\"/" Dockerfile || exit 1
+
+echo "Finished setting up the Redis Exporter ${VERSION:1} image"
