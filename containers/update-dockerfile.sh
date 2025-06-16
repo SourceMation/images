fdc8d6700c59c213ccccf891111706bcefee039a
Alex Baranowski <Alex@euro-linux.com>
2025-06-16 12:08:00 +0200

    Micro changes to update-dockerfile.sh

--- containers/update-dockerfile.sh
+++ containers/update-dockerfile.sh
@@ -15,19 +15,10 @@ BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Source the lib.sh
 . "${BASE}/lib.sh"
 
-
-# Find the names
-
-
 for i in $(find . -name 'Dockerfile' | sort); do
-    #dir_path=$(dirname "$i")
-    # Get the latest directory on the path
-    #container_dir=$(basename "$dir_path")
     container_dir=$(dirname "$i")
     print_info "Updating ${container_dir} Dockerfile"
     prepare_build || true
 done
 
-echo "The changes that might be incorporated are"
-
-git diff
+git status --porcelain
