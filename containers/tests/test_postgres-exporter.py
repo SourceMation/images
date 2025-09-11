2d9f0e1a61df9b1a499ba9ab11f86144dcfe5155
Bartosz Nyczaj <bartosznyczaj@gmail.com>
2025-09-11 12:40:53 +0200

    Use python command for file check

--- containers/tests/test_postgres-exporter.py
+++ containers/tests/test_postgres-exporter.py
@@ -1,17 +1,18 @@
 import subprocess
 import pytest
+import os
 
 def test_postgres_exporter_yml_exists():
-    result = subprocess.run(["test", "-f", "./postgres_exporter.yml"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    assert result.returncode == 0, "Postgres-exporter.yml does not exists"
+    yml_file = '/postgres_exporter.yml'
+    assert os.path.isfile(yml_file), f"Config file not found: {yml_file}"
 
 def test_postgres_exporter_executable():
-    result = subprocess.run(["test", "-f", "/bin/postgres_exporter", "&&", "test", "-x", "/bin/postgres_exporter"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    assert result.returncode == 0, "Postgres-exporter is not an executable file"
+    binary = "/bin/postgres_exporter"
+    assert os.path.isfile(binary), f"Binary not found: {binary}"
+    assert os.access(binary, os.X_OK), f"Binary is not executable: {binary}"
 
 def test_postgres_exporter_listening_on_port_9187():
     try:
-        # Check if postgres exporter is listening on port 9187
         result = subprocess.run(['ss', '-tuln'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         assert 'tcp' in result.stdout
         assert ':9187' in result.stdout, "Postgres exporter is not listening on port 9187."
