18b741b5f93cff441c0b386d7d2ac61594ac6931
Bartosz Nyczaj <bartosznyczaj@gmail.com>
2025-09-12 13:55:05 +0200

    Fix tests

--- containers/tests/test_pgpool.py
+++ containers/tests/test_pgpool.py
@@ -2,19 +2,19 @@ import subprocess
 import os
 
 def test_postgres_user_and_group_exist():
-    result = subprocess.run([" id", "postgres"], capture_output=True, text=True)
+    result = subprocess.run(["id", "postgres"], capture_output=True, text=True)
     assert result.returncode == 0, "id postgres command failed."
     assert "(postgres)" in result.stdout, "group postgres not found."
 
 def test_ownership_main_pgpool_directory():
-    result = subprocess.run(["stat", "-c", "%U", "/opt/pgpool-II"])
+    result = subprocess.run(["stat", "-c", "%U", "/opt/pgpool-II"], capture_output=True, text=True)
     assert result.returncode == 0, "stat command failed."
-    assert result.stdout == "postgres", "ownership of the main pgpool directory is invalid"
+    assert result.stdout.strip() == "postgres", "ownership of the main pgpool directory is invalid"
 
 def test_ownership_run_pgpool_directory():
-    result = subprocess.run(["stat", "-c", "%U", "/var/run/pgpool"])
+    result = subprocess.run(["stat", "-c", "%U", "/var/run/pgpool"], capture_output=True, text=True)
     assert result.returncode == 0, "stat command failed."
-    assert result.stdout == "postgres", "ownership of the run pgpool directory is invalid"
+    assert result.stdout.strip() == "postgres", "ownership of the run pgpool directory is invalid"
 
 def test_pgpool_binary_is_executable():
     binary = "/opt/pgpool-II/bin/pgpool"
