6e910fa49f45c6ec11f52c5348cbac8ef8cd27f5
Paweł Piasek <61917528+paadpi@users.noreply.github.com>
2025-09-16 12:47:13 +0200

    add etcd image (#127)
    * add etcd image
    
    * fix: WORKDIR ownership
    
    * Fix directories in tests
    
    ---------
    
    Co-authored-by: Bartosz Nyczaj <bartosznyczaj@gmail.com>

--- containers/build-container.sh
+++ containers/build-container.sh
@@ -155,6 +155,9 @@ test_container(){
             CONTAINER_RUN_COMMAND=""
             CONTAINER_RUN_PARAMETERS="-e POSTGRES_HOST_AUTH_METHOD=trust"
             ;;
+        "etcd")
+            CONTAINER_RUN_COMMAND=""
+            ;;
         "helidon")
             CONTAINER_RUN_COMMAND=""
             ;;
