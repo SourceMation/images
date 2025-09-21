5785d9b90accbdf9e5683aa5f96cd84fe186006b
Alex Baranowski <aleksander.baranowski@yahoo.pl>
2025-09-21 13:33:03 +0200

    Fix kubectl build

--- containers/images/kubectl/conf.sh
+++ containers/images/kubectl/conf.sh
@@ -1,4 +1,4 @@
 # We need to set the /bin/bash as entrypoint
-ENTRYPOINT_CMD="/bin/bash"
+ENTRYPOINT_CMD="--entrypoint /bin/bash"
 # Do not use /bin/bash as `bash bash` itself fails as /usr/bin/bash is binary
 CONTAINER_RUN_COMMAND=""
