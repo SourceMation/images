1352f7810335be1007e7b58b3fd1530b03193b0e
Michal Niezborala <michal.niezborala@linuxpolska.pl>
2025-09-26 10:07:19 +0200

    modify init.sh file for hugo

--- containers/images/hugo/init.sh
+++ containers/images/hugo/init.sh
@@ -10,14 +10,14 @@ DART_SASS="dart-sass"
 
 # Updating repository metadata and downloading latest versions
 echo "Checking the latest available version of $DART_SASS"
-DART_SASS_VERSION=$(git ls-remote --refs --tags https://github.com/sass/dart-sass.git | grep  -o 'refs/tags/[0-9]*[.0-9]*[.0-9]*$' | sort --version-sort --reverse | tr -d 'refs/tags/' | head -n 1)
+DART_SASS_VERSION=$(git ls-remote --refs --tags https://github.com/sass/dart-sass.git | grep  -Eo 'refs/tags/[0-9]+\.[0-9]+\.[0-9]+$' | sort --version-sort --reverse | tr -d 'refs/tags/' | head -n 1)
 # Exit with an error if the returned version contains anything other
 # than digits and dots
 [[ ! $DART_SASS_VERSION =~ ^[0-9.]+$ ]] && exit 1
 echo "DART_SASS_VERSION = $DART_SASS_VERSION"
 
 echo "Checking the latest available version of $APP"
-HUGO_VERSION=$(git ls-remote --refs --tags https://github.com/gohugoio/hugo.git | grep  -o 'refs/tags/v[0-9]*[.0-9]*[.0-9]*$' | sort --version-sort --reverse | tr -d 'refs/tags/v' | head -n 1)
+HUGO_VERSION=$(git ls-remote --refs --tags https://github.com/gohugoio/hugo.git | grep  -Eo 'refs/tags/v[0-9]+\.[0-9]+\.[0-9]+$' | sort --version-sort --reverse | tr -d 'refs/tags/v' | head -n 1)
 # Exit with an error if the returned version contains anything other
 # than digits and dots
 [[ ! $DART_SASS_VERSION =~ ^[0-9.]+$ ]] && exit 1
